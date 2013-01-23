672280a3211b50322df4ffe46b8f02837a7a33db
starrynte <starrynte@gmail.com>
2013-01-22 19:49:40 -0800

    Edit UI to use ListView, however that may no longer be necessary...

--- 24Solver/src/org/starrynte/solver24/MainActivity.java
+++ 24Solver/src/org/starrynte/solver24/MainActivity.java
@@ -1,21 +1,31 @@
 package org.starrynte.solver24;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity
 {
 	public final static String EXTRA_NUMBERS = "org.starrynte.solver24.NUMBERS";
+	ArrayAdapter<Integer> adapter;
+	List<Integer> numberList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		numberList = new ArrayList<Integer>();
+		adapter = new ArrayAdapter<Integer>(this, R.layout.item_number, numberList);
+		((ListView) findViewById(R.id.number_list)).setAdapter(adapter);
 	}
 
 	@Override
@@ -26,8 +36,16 @@ public class MainActivity extends Activity
 		return true;
 	}
 
+	public void add(View view)
+	{
+		EditText number = (EditText) findViewById(R.id.number_input);
+		adapter.add(Integer.valueOf(number.getText().toString()));
+		number.setText("");
+	}
+
 	public void calculate(View view)
 	{
+		/*
 		EditText numberList = (EditText) findViewById(R.id.number_list);
 		String[] numbersRaw = numberList.getText().toString().trim().split("\\s+");
 		int[] numbers = new int[numbersRaw.length];
@@ -38,6 +56,7 @@ public class MainActivity extends Activity
 		Intent results = new Intent(this, ResultsActivity.class);
 		results.putExtra(EXTRA_NUMBERS, numbers);
 		startActivity(results); //consider startActivityForResult
+		*/
 	}
 
 }
